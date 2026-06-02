d8b76a6f03b784ccb4776fafd777ffcc79483b91
heyz-hye <158526293+heyz-hye@users.noreply.github.com>
2026-06-02 13:34:47 -0400

    Add: boats-to-save-people - submission-3

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-3.py
@@ -0,0 +1,36 @@
+'''
+sort the list in ascending order
+we should have a left and right pointer, with left pointing to the lightest and the right pointing
+to the heaviest.
+match the heaviest to the lightest if that doesn't work then the heaviest should be on the boat on its own
+then we move to the second heaviest, if they match we should count them as one boat.
+if the
+sorted dont modify original list while sort does
+
+'''
+
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        left=0
+        right=len(people)-1
+        count=0
+
+        people.sort()
+
+        while left<=right:
+            if people[left]+people[right]<=limit:
+                right-=1
+                left+=1
+                count+=1
+            elif left==right:
+                count+=1
+                left+=1
+                right-=1
+                
+            else:
+                right-=1
+                count+=1
+
+        return count
+            
+        
