339c4572529adc0e5bf72793122a1c21b6a595cb
heyz-hye <158526293+heyz-hye@users.noreply.github.com>
2026-06-04 13:01:15 -0400

    Add: binary-tree-diameter - submission-20

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-20.py
@@ -0,0 +1,32 @@
+'''
+So the idea of the problem is pretty tricky for it to be classify as an easy
+you want to calculate the diamater of each node and compare them throughout using
+a max_diameter global variable. To find the maximum diameter of a node you need to find the maximum of node.left
+height and the maximum of node.right's height. You then add 1+ both maximum to calculate the maximum diameter
+of that node. Then you can return the maximum diameter since you go through every node. Run time should be O(n)
+space complexity O(h)
+
+also remember if you can store the recursion result in a variable so you don't have to compute twice by computing
+the same thing again this save a lot of time
+
+Also this is not an easy
+Note: I confuse maximum diameter of a node with the maximum height of that node. These two are completely
+different things don't confuse them
+
+'''
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.max_diameter = 0 
+
+        def dfs(node):
+            if not node:
+                return 0
+            left=dfs(node.left)
+            right=dfs(node.right)
+            current_diameter=left+right #you compute the diameter
+            self.max_diameter=max(self.max_diameter, current_diameter) #you compare diameter of all the nodes
+            
+            return 1+max(left,right) #you set the height of the node
+
+        dfs(root)
+        return self.max_diameter
