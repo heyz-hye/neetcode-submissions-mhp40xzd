2676d4514ab5b7dbbb3ddfb3a552c62c0a657a70
heyz-hye <158526293+heyz-hye@users.noreply.github.com>
2026-06-09 23:19:54 -0400

    Add: binary-tree-right-side-view - submission-10

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-10.py
@@ -0,0 +1,33 @@
+'''
+can do a bfs and return the last element of every level
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        
+        ans=[]
+        q=deque([root])
+        
+        while q:
+            s=len(q)
+            for i in range(len(q)):           
+                node=q.popleft()
+                if i==s-1:
+                    ans.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+        return ans
+
+        
+        
