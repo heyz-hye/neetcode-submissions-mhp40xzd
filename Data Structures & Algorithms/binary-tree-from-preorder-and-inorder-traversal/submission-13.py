e8daf72ca9e05c5ad1764d76b1ac440b1eb19350
heyz-hye <158526293+heyz-hye@users.noreply.github.com>
2026-06-13 13:07:56 -0400

    Add: binary-tree-from-preorder-and-inorder-traversal - submission-13

---
+++ Data Structures & Algorithms/binary-tree-from-preorder-and-inorder-traversal/submission-13.py
@@ -0,0 +1,31 @@
+'''
+zip create tuples
+you can dict(a list of tuples)
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        smap={values:index for index, values in enumerate(inorder)}
+        self.preorderindex=0
+        def left2right(left, right)->TreeNode:
+            if left >right:
+                return None #left>right no nodes left to append
+            mid=smap[preorder[self.preorderindex]]
+            root=TreeNode(inorder[mid])
+            self.preorderindex+=1 #need to move pointer since we are not slicing any
+                                  #any array we keep our original array.
+                                  #need to increment the index pointer since 0 is always
+                                  #pointing the the root of the entire tree
+
+            root.left=left2right(left,mid-1)
+            root.right=left2right(mid+1,right)
+            return root
+        return left2right(0,len(inorder)-1)
+
+        
