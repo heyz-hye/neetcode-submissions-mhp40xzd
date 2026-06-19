c717593f7ab4a1d261b61b4b976cdb647e836a49
heyz-hye <158526293+heyz-hye@users.noreply.github.com>
2026-06-19 12:42:15 -0400

    Add: permutations - submission-5

---
+++ Data Structures & Algorithms/permutations/submission-5.py
@@ -0,0 +1,19 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        
+        perms=[[]]
+        for n in nums:
+            perm=[]
+            for p in perms: #perm is a temporary list that store our incomplete array list
+                            #array it need to be reset for every n
+                for i in range(len(p)+1):
+                    pcopy=p.copy()
+                    pcopy.insert(i,n)
+                    perm.append(pcopy)
+            perms=perm
+        return perms
+
+
+
+
+        
