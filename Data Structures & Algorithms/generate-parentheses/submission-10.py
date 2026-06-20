bbf3d1ebf89feca84f805b60904791fecdd534a0
heyz-hye <158526293+heyz-hye@users.noreply.github.com>
2026-06-20 13:48:40 -0400

    Add: generate-parentheses - submission-10

---
+++ Data Structures & Algorithms/generate-parentheses/submission-10.py
@@ -0,0 +1,40 @@
+'''
+we can keep track of left parenthesis, we can also use an hashmap, if 
+the parenthesis the opening part or the closing part more than the n you return
+in a decision you can choose to take the opening or clos
+edge you can never have a close parenthesis at the beginning
+'''
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        self.res=[]
+        subset=[]
+        if n<1:
+            return self.res
+        def dfs(paren,op,cl):
+            if paren=="(":
+                op+=1
+                subset.append(paren)
+            else:
+                cl+=1
+                subset.append(paren)
+            
+            if op==n and cl==n:
+                self.res.append("".join(subset.copy()))
+                subset.pop()
+                return
+            
+            if op>cl:
+                dfs(")",op,cl)
+               
+            
+            if op>n:
+                subset.pop()
+                return
+            else:
+                dfs("(",op,cl)
+            subset.pop()
+            
+        dfs("(",0,0)
+        return self.res
+
+        
