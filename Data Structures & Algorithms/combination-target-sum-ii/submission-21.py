a81e4ae0d4d5c04ad21ee8d29c42efffbeba7a1c
heyz-hye <158526293+heyz-hye@users.noreply.github.com>
2026-07-28 22:56:27 -0400

    Add: combination-target-sum-ii - submission-21

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-21.py
@@ -0,0 +1,21 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        res=[]
+        candidates.sort()
+        def dfs(i,cur,total):
+            if total==target:
+                res.append(list(cur)) # this make a copy of cur that fit the target since cur get change the value will get lost, list take only one argument
+                return
+
+            for j in range(i,len(candidates)):
+                if j>i and candidates[j]==candidates[j-1]:
+                    continue
+                if candidates[j]+total>target:
+                    break
+                cur.append(candidates[j])
+                dfs(j+1,cur,total+candidates[j])
+                cur.pop() #this pop micmic the descision tree of chosing it and not it at the same time
+        dfs(0,[],0)
+        return res
+
+        
